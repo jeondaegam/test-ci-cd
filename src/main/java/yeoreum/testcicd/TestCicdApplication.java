bbe75ec5f4450cfeaa57dd7226503d8017b541b5
yeoreum <jeondaegam@naver.com>
2025-12-29 17:08:20 +0900

    스프링부트 프로젝트 초기설정

---
+++ src/main/java/yeoreum/testcicd/TestCicdApplication.java
@@ -0,0 +1,13 @@
+package yeoreum.testcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yeoreum/testcicd/TestCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package yeoreum.testcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
